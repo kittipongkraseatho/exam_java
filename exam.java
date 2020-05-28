e70dc15262448effc598994c55b56a3747743da0
Kittipong_MaTiNo <kittipong.kr.57@ubu.ac.th>
2020-05-29 00:17:59 +0700

    Add files via upload
    ทดสอบ exam

---
+++ exam.java
@@ -0,0 +1,20 @@
+
+public class exam {
+	public static void main(String[] args) {
+		int z = 0;
+		int j = 1;
+		while (j < 10) {
+			z=z+1;
+			int k = 1;
+			z = z+1;
+			while (k<10) {
+				k=k+1;
+				z=z+1;
+			}
+			j=j+1;
+			
+		}
+		System.out.println("z = " +z);
+		System.out.println("stop");
+	}
+}
